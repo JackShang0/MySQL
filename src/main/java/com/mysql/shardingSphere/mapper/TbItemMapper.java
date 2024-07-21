51ff5d916c5747486a2d99e24613de85a6754ec2
shangdj <15091520295@163.com>
2024-07-21 20:30:01 +0800

    搭建主从集群，完成读写分离，负载均衡配置

---
+++ src/main/java/com/mysql/shardingSphere/mapper/TbItemMapper.java
@@ -0,0 +1,9 @@
+package com.mysql.shardingSphere.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mysql.shardingSphere.dto.TbItem;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TbItemMapper extends BaseMapper<TbItem> {
+}
---
+++ src/test/java/com/mysql/shardingSphere/ReadWriteTest.java
@@ -0,0 +1,44 @@
+package com.mysql.shardingSphere;
+
+import com.mysql.Application;
+import com.mysql.shardingSphere.dto.TbItem;
+import com.mysql.shardingSphere.mapper.TbItemMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ReadWriteTest {
+
+    @Resource
+    TbItemMapper tbItemMapper;
+
+    /**
+     * 插入数据，确认数据是否主从同步
+     */
+    @Test
+    public void testInsert(){
+        tbItemMapper.insert(new TbItem().setCategory("222").setBrand("111").setStatus("000"));
+    }
+
+
+    /**
+     * 测试事务
+     */
+    @Transactional
+    @Test
+    public void testTrans(){
+        tbItemMapper.insert(new TbItem().setCategory("333").setBrand("333").setStatus("1"));
+        List<TbItem> tbItems = tbItemMapper.selectList(null);
+        System.out.println("tbItems = " + tbItems);
+    }
+}
